958f303c14f349b7725995f28cbb57c1c34140c5
lennon <1099571219@qq.com>
2022-02-23 21:20:19 +0800

    使用 ts 前 测试

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,3 +3,13 @@
         <div>footer</div>
     </footer>
 </template>
+<style lang="less" scoped>
+footer{
+    align-self: end;
+    background-color: #d7d7d7;
+    color: #666;
+    font-size: 13px;
+    padding: 10px;
+    text-align: center;
+}
+</style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,45 @@
 <template>
   <header>
-    <h1>Let's share</h1>
-    <p>精品博客汇聚</p>
-    <div class="btns">
-      <el-button>立即登录</el-button>
-      <el-button>注册账号</el-button>
-    </div>
+    <template v-if="false">
+      <h1>Let's share</h1>
+      <p>精品博客汇聚</p>
+      <div class="btns">
+        <el-button>立即登录</el-button>
+        <el-button>注册账号</el-button>
+      </div>
+    </template>
+    <template>
+      <h1>Let's share</h1>
+      <i class="edit el-icon-edit"></i>
+      <img class="avatar" src="" />
+    </template>
   </header>
 </template>
-<style lang="less">
+<script >
 
-header{
-  padding:0 12% 30px 12%;
-  
+</script>
+<style lang="less">
+@import "../assets/style/base.less";
+header {
+  padding: 0 12% 30px 12%;
+  background: @bgColor;
+  display: grid;
+  justify-items: center;
+  h1 {
+    color: #fff;
+    font-size: 40px;
+    margin: 60px 0 0 0;
+    text-transform: uppercase;
+  }
+  p {
+    margin: 15px 0 0 0;
+    color: #fff;
+  }
+  .btns {
+    margin-top: 20px;
+  }
+  button {
+    margin: 20px 5px 0;
+  }
 }
 </style>
