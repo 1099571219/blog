c7f05300a7ddb08d9fa60edcd9aef5b858a57d93
lennon <1099571219@qq.com>
2022-02-27 20:42:23 +0800

    完成CSS

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,12 +4,12 @@
     </footer>
 </template>
 <style lang="less" scoped>
-footer{
-    align-self: end;
-    background-color: #d7d7d7;
-    color: #666;
-    font-size: 13px;
-    padding: 10px;
-    text-align: center;
+footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
 }
 </style>
